f92f35630b539851439f5052bf4ca53a6ea65829
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2021-07-29 09:19:24 +0300

    Delete & Inserting Item to Array

---
+++ arrays.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int CAP = 10;
+
+
+void put(int index , int item, int *arr){
+
+
+    for (int i = CAP - 1; i >= index; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+    arr[index] = item;
+    CAP++;
+    
+}
+
+void deleteItem(int index , int *arr)
+{
+
+
+    for (int i = index; i < CAP; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    CAP--;
+
+}
+
+
+
+
+int main()
+{
+    int list[CAP] = { 12, 34, 45, 4, 8, 10, 16, 180, 182, 22 };
+
+
+    put(2, 99, list);
+
+    deleteItem(1, list);
+    deleteItem(8, list);
+    deleteItem(4, list);
+
+    put(3, 9, list);
+
+
+    cout << "New List: " << endl;
+
+    for (int i = 0; i < CAP; i++)
+    {
+        cout << list[i] << endl;
+    }
+
+
+
+
+    return 0;
+}
