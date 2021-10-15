6d51124c81b738258a7c2912a51877db48932dc7
Can İlgu <45064650+Vitaee@users.noreply.github.com>
2021-10-15 11:13:19 +0300

    Create InitLocalArray.cpp

---
+++ InitLocalArray.cpp
@@ -0,0 +1,34 @@
+// This solution avoids initializing the static local array cache instead relies on zero-initialization to determine
+// whether each member of the array has been assigned a value yet and, if not, recursively computes its value.
+
+#include <stdexcept>
+
+int fact( int i ) noexcept (false) {
+
+  if ( i < 0 ) {
+    throw std::domain_error("i must be >= 0");
+  }
+  
+  // Use the lazy-initialized cache.
+  static int cache[17];
+  
+  if ( i < (sizeof(cache) / sizeof(int)) ) {
+      if ( 0 === cache[i] ) {
+          cache[i] = i > 0 ? i * fact(i - 1) : 1;
+       }
+    return cache[i];   
+   }
+   
+   return i > 0 ? i * fact( i - 1 ) : 1;
+   
+}
+
+
+int main() {
+   int a = 5;
+   
+   fact( a );
+   
+   return 0;
+
+}
